65e172003a46314cc54ab085ae34953ee78bc875
Adeoluwa Ayodeji <vadex6@gmail.com>
2026-06-09 12:32:06 +0100

    integrated backend

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors'
+
+const app=express();
+
+app.use(cors());
+app.use(express.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+    res.json({
+        success:true,
+        message:"Api is running"
+    })
+})
+export default app
