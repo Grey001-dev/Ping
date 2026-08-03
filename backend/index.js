b20ee27427d840edb2d242049c09c13937619584
Grey001-dev <vadex6@gmail.com>
2026-08-03 18:40:22 +0100

    added new domain name to backend

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ export const io=new Server(httpServer,{
 app.use(cookieParser())
 app.use(cors({
     origin:process.env.NODE_ENV==='production'
-    ? 'https://ping001.netlify.app'
+    ? 'https://ping-two-orpin.vercel.app'
     : 'http://localhost:5173'
     ,
     credentials:true,
